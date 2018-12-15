400c47b87a0c97ae3a31c2dd5806d08002bbb6c7
April Copes <antdevelopment1@gmail.com>
2018-12-14 21:45:44 -0500

    Adds How to play outline

---
+++ src/HowToPlay.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HowToPlay = () => {
+    return (
+        <div>
+            <p>
+                Step 1
+                Step 2
+            </p>
+        </div>
+    )
+}
+
+export default HowToPlay;

ebf54afa037f4cc7737373c2f1e61ca7a88e70f4
April Copes <antdevelopment1@gmail.com>
2018-12-16 14:50:40 -0500

    Adds text component to render after click

---
+++ src/Text.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Text = () => {
+    return (
+        <div>
+            <h1 className="rules"> Rules of the game:</h1>
+            <p className="instructions">A word will display on the screen. You will have 60 seconds to guess as many words as you can.
+            You have to rely on your team to give you clues about the word. Your team cannot use the word itself as a hint. Yada yada yada
+            </p>
+        </div>
+    )
+}
+
+export default Text;

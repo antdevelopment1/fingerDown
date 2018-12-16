5d9bdbc9cee761e5e1095fcc62b1f8be3fba35cd
April Copes <antdevelopment1@gmail.com>
2018-12-15 23:13:05 -0500

    commented

--- src/HowToPlay.js
+++ src/HowToPlay.js
@@ -1,14 +1,14 @@
-import React from 'react';
+// import React from 'react';
 
-const HowToPlay = () => {
-    return (
-        <div>
-            <p>
-                Step 1
-                Step 2
-            </p>
-        </div>
-    )
-}
+// const HowToPlay = () => {
+//     return (
+//         <div>
+//             <p>
+//                 Step 1
+//                 Step 2
+//             </p>
+//         </div>
+//     )
+// }
 
-export default HowToPlay;
+// export default HowToPlay;

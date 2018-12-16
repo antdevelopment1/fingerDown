d940ee8f6b5b5d1e8470a9c40f3c36a91fd1cab4
April Copes <antdevelopment1@gmail.com>
2018-12-16 14:48:00 -0500

    removes unneeded code

--- src/index.js
+++ src/index.js
@@ -4,31 +4,12 @@ import styles from './styles.css';
 import Title from './Title';
 
 class App extends Component {
-    state = {
-        howToPlayMessage: ''
-    }
-
-    onClickSubmit = () => {
-
-         this.setState({
-            howToPlayMessage: 'hello how are you'
-        })
-        // return (
-        //     <div>
-        //         this.state.howToPlayMessage;
-        //     </div>
-        // )
-        
-        
-    }
+    
     render() {
         return(
             <div>
-                  <Title howtoplay={this.onClickSubmit}/>
-
+                  <Title />
             </div>
-          
-
         )
     }
 }
